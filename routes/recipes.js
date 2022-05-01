00eb1b12552297ffdf9a998d786c5cf00dfdf0fd
adarsh-yara <adarsh.kumar@yara.com>
2022-05-01 10:45:45 +0530

    test_01

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,27 @@
-var recipes = require('../recipes.json');
-var router = require('express').Router();
+var recipes = require("../recipes.json");
+var router = require("express").Router();
 
+router.get("/shopping-list", (req, res) => {
+  console.log(` RRR ${req}`);
+  const ids = req.query.ids || [];
+
+  if (ids && ids.length === 0) {
+    res.status(400).send();
+    return;
+  }
+  let ingredientArray = [];
+  const idsArray = ids.split(",");
+  for (const recipe of recipes) {
+    if (idsArray && idsArray.includes(recipe.id.toString())) {
+      ingredientArray = [...ingredientArray, ...recipe.ingredients];
+    }
+  }
+  if (ingredientArray.length > 0) {
+    res.status(200).send(ingredientArray);
+    return;
+  } else {
+    res.status(404).send("NOT_FOUND");
+    return;
+  }
+});
 module.exports = router;
-
